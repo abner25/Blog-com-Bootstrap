6d54f2fa41404d3fc4a57b57157d3e5dd93a3af3
abner <abnerrds25@gmail.com>
2021-10-06 20:10:49 -0300

    Excluindo comentários

--- app/policies/comment_policy.rb
+++ app/policies/comment_policy.rb
@@ -2,4 +2,9 @@ class CommentPolicy < ApplicationPolicy
   def create?
     user&.id
   end
+
+  # O mesmo usuário  = mesmo usuário do comentário
+  def destroy?
+    user&.id == record.user.id
+  end
 end
